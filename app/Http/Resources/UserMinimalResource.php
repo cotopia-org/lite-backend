f7beb5ee75f224bded69b9a6a97a8da7f6532268
Katerou22 <xkaterou22@gmail.com>
2024-09-02 17:08:02 +0330

    fix ghost.

--- app/Http/Resources/UserMinimalResource.php
+++ app/Http/Resources/UserMinimalResource.php
@@ -5,17 +5,20 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserMinimalResource extends JsonResource {
+class UserMinimalResource extends JsonResource
+{
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array {
+    public function toArray(Request $request): array
+    {
         return [
             'id'          => $this->id,
             'name'        => $this->name,
             'username'    => $this->username,
+            'status'      => $this->status,
             'avatar'      => FileResource::make($this->avatar),
             'coordinates' => $this->coordinates,
 
