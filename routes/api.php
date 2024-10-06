0470c10b50bcaec9e70247dbec789d9b30c0d56a
katerou22 <xkaterou22@gmail.com>
2024-10-06 11:54:37 +0330

    FIx livekit.

--- routes/api.php
+++ routes/api.php
@@ -75,10 +75,10 @@ Route::middleware('auth:sanctum')->group(callback: function () {
 
     Route::controller(RoomController::class)->prefix('rooms')->group(function () {
         Route::post('/', 'create');
+        Route::get('/leave', 'leave');
         Route::get('/{room}/', 'get');
         Route::put('/{room}/', 'update');
         Route::get('/{room}/join', 'join');
-        Route::get('/leave', 'leave');
         Route::delete('/{room}', 'delete');
 
     });
