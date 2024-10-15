63073f4435f0ab872096121bbd95aabad4646bf9
katerou22 <xkaterou22@gmail.com>
2024-10-15 22:26:13 +0330

    Remove unneccessary sockets.

--- app/Http/Resources/UserMinimalResource.php
+++ app/Http/Resources/UserMinimalResource.php
@@ -5,19 +5,18 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserMinimalResource extends JsonResource
-{
+class UserMinimalResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
+    public function toArray(Request $request): array {
         return [
             'id'          => $this->id,
             'name'        => $this->name,
             'username'    => $this->username,
+            'room_id'     => $this->room_id,
             'status'      => $this->status,
             'avatar'      => FileResource::make($this->avatar),
             'coordinates' => $this->coordinates,
