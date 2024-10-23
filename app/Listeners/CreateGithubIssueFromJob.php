64a723ef2d705aee5fd417a2d767ca7e9f00d94f
Katerou22 <xkaterou22@gmail.com>
2024-10-23 17:13:25 +0330

    Disable github for now.

--- app/Listeners/CreateGithubIssueFromJob.php
+++ app/Listeners/CreateGithubIssueFromJob.php
@@ -21,15 +21,15 @@ class CreateGithubIssueFromJob
      */
     public function handle(JobCreated $event): void
     {
-        $job = $event->job;
-        $issue_repos = config('github.issue_repositories');
-        $githubSrv = $this->githubService();
-        foreach ($issue_repos as $repo) {
-            $githubSrv->createIssue($repo, [
-                'title'       => $job->title,
-                'description' => $job->description,
-            ]);
-        }
+//        $job = $event->job;
+//        $issue_repos = config('github.issue_repositories');
+//        $githubSrv = $this->githubService();
+//        foreach ($issue_repos as $repo) {
+//            $githubSrv->createIssue($repo, [
+//                'title'       => $job->title,
+//                'description' => $job->description,
+//            ]);
+//        }
     }
 
     public function githubService(): Github
