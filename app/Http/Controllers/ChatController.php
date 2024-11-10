b21dcb99f0006358bbefe4396faf3cede3f653f9
katerou22 <xkaterou22@gmail.com>
2024-11-10 15:55:59 +0330

    Fix chat with created_at.

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -19,7 +19,7 @@ class ChatController extends Controller {
         $request->validate([
 
 
-                               'user_id' => 'required|exists:users',
+                               'user_id' => 'required|exists:users,id',
                            ]);
 
 
