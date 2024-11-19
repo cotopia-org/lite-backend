428deff22f20ac395bc7bf5837a11332237c3be3
katerou22 <xkaterou22@gmail.com>
2024-11-19 22:55:30 +0330

    No need socket id for disconnected.

--- app/Utilities/helpers.php
+++ app/Utilities/helpers.php
@@ -7,30 +7,8 @@ use App\Models\Message;
 use App\Models\User;
 use App\Utilities\Constants;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Redis;
 
-function getPhoneNumber($phone) {
-    if ($phone === NULL) {
-        return NULL;
-    }
-    // Remove any non-digit characters
-    $phone = convert($phone);
-
-    $phone = preg_replace('/\D/', '', convert($phone));
-
-    // Check if the number starts with '0098' or '+98' and remove it
-    if (preg_match('/^(00|\+)98|0/', $phone)) {
-        $phone = preg_replace('/^(00|\+)98|^0/', '', $phone);
-    }
-
-    // Add the country code '98' if it's missing
-    if (!preg_match('/^98/', $phone)) {
-        $phone = '98' . $phone;
-    }
-
-    return $phone;
-}
 
 function sendSocket($eventName, $channel, $data) {
     if ($channel !== NULL) {
@@ -42,6 +20,16 @@ function sendSocket($eventName, $channel, $data) {
     }
 
 
+}
+
+function updateMesssage($message, $text, $reply_to = NULL) {
+
+    $message->update([
+                         'text'      => $text,
+                         'is_edited' => TRUE,
+                         'reply_to'  => $reply_to,
+
+                     ]);
 }
 
 function sendMessage($message, $chat_id, $reply_to = NULL) {
@@ -77,33 +65,14 @@ function joinUserToSocketRoom($user_id, $room_id) {
                                              'room_id' => $room_id
                                          ]));
 
-    //    return Http::post(env('SOCKET_URL', 'http://localhost:3010') . '/joinToRoom', [
-    //        'data' => [
-    //            'user_id' => $user_id,
-    //            'room_id' => $room_id
-    //        ]
-    //    ])->json();
 
 }
 
-
-function sendSms($phone, $code) {
-    return Http::asForm()->withHeader('apikey', '001a87a26baf886222895114bff20fcde5a54706f09e22487645b422fbd4dd15')
-               ->post('https://api.ghasedak.me/v2/verification/send/simple', [
-                   'param1'   => $code,
-                   'template' => 'resanaAuth',
-                   'type'     => '1',
-                   'receptor' => $phone,
-               ])->json();
-
-    //TODO: // Have to go in queue.
-}
 
 function get_enum_values($cases, $key = FALSE): array {
     return array_column($cases, 'value', $key ? 'name' : NULL);
 }
 
-/*---------------------------------------------------------------------API--------------------------------------------------------------------------------------------*/
 
 function api($data = NULL, $message = Constants::API_SUCCESS_MSG, $code = 1000, $http_code = 200): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory {
     if ($message === Constants::API_SUCCESS_MSG) {
