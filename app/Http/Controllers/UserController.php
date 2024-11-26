5678006d21363bb3cc91f91ebe9f65ef558c7c3d
Katerou22 <xkaterou22@gmail.com>
2024-11-26 12:56:48 +0330

    Test user chats order.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -265,8 +265,7 @@ class UserController extends Controller
                                             ->where("mentions.mentionable_id", $user->id);
                                     },
                                 ])
-                    ->orderBy('lastMessage.created_at', 'DESC')
-                    ->get()
+                    ->get()->sortByDesc('lastMessage.created_at')
             )
         );
     }
