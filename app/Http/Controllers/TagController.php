121207b5222239f5deb67b11ae9ffc0940b60e61
katerou22 <xkaterou22@gmail.com>
2024-11-30 18:27:44 +0330

    Fix tag delete.

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -56,7 +56,7 @@ class TagController extends Controller {
 
 
         $tag->users()->detach();
-        $tag->delete();
+        $tag->update(['workspace_id' => NULL]);
         return api(TRUE);
     }
 
