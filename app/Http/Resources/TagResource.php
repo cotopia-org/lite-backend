5e6296e41031627a0079b394b90298276034593a
katerou22 <xkaterou22@gmail.com>
2024-11-30 15:33:59 +0330

    Fix tag resource.

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -5,15 +5,19 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TagResource extends JsonResource
-{
+class TagResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
+    public function toArray(Request $request): array {
+        return [
+            'id'           => $this->id,
+            'workspace_id' => $this->workspace_id,
+            'title'        => $this->title,
+            'members'      => UserMinimalResource::collection($this->users),
+            'created_at'   => $this->created_at
+        ];
     }
 }
