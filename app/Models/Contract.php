723c200dd63b57fec0a1b42eb10a64d5d71ee608
katerou22 <xkaterou22@gmail.com>
2024-12-08 22:32:08 +0330

    Test Renew Contract.

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -36,8 +36,8 @@ class Contract extends Model {
         'end_at'   => 'datetime',
     ];
 
+    protected $dates = ['start_at', 'end_at'];
 
-    protected array $dates = ['start_at', 'end_at'];
 
     public function user() {
         return $this->belongsTo(User::class);
@@ -47,9 +47,9 @@ class Contract extends Model {
 
 
         $attrs = $this->toArray();
-        dd($attrs['start_at']);
-        $attrs['start_at'] = $attrs['start_at']->addMonth();
-        $attrs['end_at'] = $attrs['end_at']->addMonth();
+        dd($this->start_at);
+        $attrs['start_at'] = $this->start_at->addMonth();
+        $attrs['end_at'] = $this->end_at->addMonth();
         $contract = self::create($attrs);
 
 
