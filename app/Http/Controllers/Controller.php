4361aa47672a82dde4ecbc4277218fdbe6c79812
katerou22 <xkaterou22@gmail.com>
2024-12-09 18:23:42 +0330

    Fix mentionedJobs.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,18 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\Auth\Authenticatable;
 
 abstract class Controller extends \Illuminate\Routing\Controller {
-    protected Authenticatable $user;
 
-    /**
-     * @throws \Exception
-     */
-    public function __construct() {
-        $user = auth()->user();
-
-
-        $this->user = $user;
-    }
 }
