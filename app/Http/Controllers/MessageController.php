ff9dce75d386c2080fd3fb56efceaee5e2540285
katerou22 <xkaterou22@gmail.com>
2024-12-11 03:02:25 +0330

    Fix sendMessage.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -73,8 +73,8 @@ class   MessageController extends Controller {
                                                                                                          ]);
 
 
-        $res = MessageResource::make($message);
-        sendSocket('messageReceived', 'chat-' . $request->chat_id, $res);
+        //        $res = MessageResource::make($message);
+        //        sendSocket('messageReceived', 'chat-' . $request->chat_id, $res);
         return api(MessageResource::make($message));
 
     }
