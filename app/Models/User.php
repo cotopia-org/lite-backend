0b7d57b1bc5ca263ab83bf7a0173d3f99801e30f
katerou22 <xkaterou22@gmail.com>
2024-12-11 01:32:10 +0330

    Fix not getting my jobs in mentions again.

--- app/Models/User.php
+++ app/Models/User.php
@@ -131,7 +131,7 @@ class User extends Authenticatable {
 
     public function jobs() {
         return $this
-            ->belongsToMany(Job::class)->withPivot('role', 'status')->wherePivot('status', '!=', Constants::DISMISSED);
+            ->belongsToMany(Job::class)->withPivot('role', 'status')->wherePivotNotIn('status', [Constants::DISMISSED]);
     }
 
     public function roles(): \Illuminate\Database\Eloquent\Relations\BelongsToMany {
