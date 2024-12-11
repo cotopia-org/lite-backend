1e350d57e391b97f88aa9e1d016fe03cdbc37c6f
katerou22 <xkaterou22@gmail.com>
2024-12-11 09:28:30 +0330

    Fix owner as super-admin

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -23,7 +23,7 @@ class PaymentResource extends JsonResource {
             'round'       => $this->round,
             'type'        => $this->type,
             'user'        => UserSuperMinimalResource::make($this->user),
-            'contract_id' => $this->contract_id
+            'contract_id' => ContractResource::collection($this->contract)
         ];
     }
 }
