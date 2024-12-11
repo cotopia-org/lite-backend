dcfb98bbe702a39f3825cd367edda9a23ca6b1ae
katerou22 <xkaterou22@gmail.com>
2024-12-12 00:51:36 +0330

    Fix get activities.

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -14,14 +14,15 @@ class ActivityController extends Controller {
             ->activities()->orderBy('id', 'ASC')->where('created_at', '>=', $request->start_at)
             ->where('created_at', '<=', $request->end_at)->get()->map(function ($activity) {
                 return [
-                    'id'           => $activity->id,
-                    'join_at'      => $activity->join_at->timezone('Asia/Tehran')->toDateTimeString(),
-                    'left_at'      => $activity->left_at?->timezone('Asia/Tehran')->toDateTimeString(),
-                    'job_id'       => $activity->job_id,
-                    'room_id'      => $activity->room_id,
-                    'workspace_id' => $activity->workspace_id,
-                    'diff'         => $activity->join_at?->diffInMinutes($activity->left_at),
-                    'data'         => $activity->data,
+                    'id'              => $activity->id,
+                    'join_at'         => $activity->join_at->timezone('Asia/Tehran')->toDateTimeString(),
+                    'left_at'         => $activity->left_at?->timezone('Asia/Tehran')->toDateTimeString(),
+                    'job_id'          => $activity->job_id,
+                    'room_id'         => $activity->room_id,
+                    'workspace_id'    => $activity->workspace_id,
+                    'diff_in_minutes' => $activity->join_at?->diffInMinutes($activity->left_at),
+                    'diff_in_hours'   => $activity->join_at?->diffInHours($activity->left_at),
+                    'data'            => $activity->data,
 
                 ];
             });
