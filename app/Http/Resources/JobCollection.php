e131ce3f998ab2b8a123d31bb69c7daea23548e8
katerou22 <xkaterou22@gmail.com>
2024-12-14 16:27:33 +0330

    Fix paginate for workspace jobs.

--- app/Http/Resources/JobCollection.php
+++ app/Http/Resources/JobCollection.php
@@ -5,17 +5,17 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class JobCollection extends ResourceCollection
-{
+class JobCollection extends ResourceCollection {
     /**
      * Transform the resource collection into an array.
      *
      * @return array<int|string, mixed>
      */
-    public function toArray(Request $request): array
-    {
+    public function toArray(Request $request): array {
+        dd($this);
         return [
             'data' => JobResource::collection($this->collection),
+
             'meta' => $this->meta
         ];
     }
