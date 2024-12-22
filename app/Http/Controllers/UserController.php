93ec236e8aa784c4307bf6f203627494ebc9b472
katerou22 <xkaterou22@gmail.com>
2024-12-22 11:07:24 +0330

    Test Fulfillment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -103,6 +103,7 @@ class UserController extends Controller {
 
         $sum_minutes = 0;
         $schedule_total = $user->getScheduledHoursInWeek();
+        dd($schedules);
         foreach ($schedules as $schedule) {
             $acts = $activities
                 ->where("left_at", ">=", $schedule["start"])->where("join_at", "<=", $schedule["end"]);
