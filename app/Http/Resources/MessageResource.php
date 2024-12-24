c2b72336957fbbecfa972b86a8c15e2d4bab1de2
katerou22 <xkaterou22@gmail.com>
2024-12-24 20:29:29 +0330

    Fix translated text in messages reousrce.

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -14,23 +14,24 @@ class MessageResource extends JsonResource {
     public function toArray(Request $request): array {
 
         return [
-            'id'         => $this->id,
-            'user'       => $this->user_id,
-            'text'       => $this->deleted_at === NULL ? $this->text : 'This message has been deleted',
-            'files'      => FileResource::collection($this->files),
-            'chat_id'    => $this->chat_id,
-//            'seen'       => $this->saw(auth()->user()),
-            'nonce_id'   => (int)$this->nonce_id,
-            'is_edited'  => $this->is_edited,
-            'is_pinned'  => $this->is_pinned,
-            'reply_to'   => self::make($this->replyTo),
-            'mentions'   => MentionResource::collection($this->mentions),
-            'links'      => $this->links,
-            'reacts'     => $this->reacts,
-            'created_at' => $this->created_at->timestamp,
-            'updated_at' => $this->updated_at?->timestamp,
-            'deleted_at' => $this->deleted_at?->timestamp,
-            'seens'      => $this->seens()
+            'id'              => $this->id,
+            'user'            => $this->user_id,
+            'text'            => $this->deleted_at === NULL ? $this->text : 'This message has been deleted',
+            'translated_text' => $this->translated_text,
+            'files'           => FileResource::collection($this->files),
+            'chat_id'         => $this->chat_id,
+            //            'seen'       => $this->saw(auth()->user()),
+            'nonce_id'        => (int)$this->nonce_id,
+            'is_edited'       => $this->is_edited,
+            'is_pinned'       => $this->is_pinned,
+            'reply_to'        => self::make($this->replyTo),
+            'mentions'        => MentionResource::collection($this->mentions),
+            'links'           => $this->links,
+            'reacts'          => $this->reacts,
+            'created_at'      => $this->created_at->timestamp,
+            'updated_at'      => $this->updated_at?->timestamp,
+            'deleted_at'      => $this->deleted_at?->timestamp,
+            'seens'           => $this->seens()
 
         ];
     }
