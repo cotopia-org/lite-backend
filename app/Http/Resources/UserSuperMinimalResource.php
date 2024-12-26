d59505679e5193f0adf64c42bf8e4438be7e4003
katerou22 <xkaterou22@gmail.com>
2024-12-26 23:41:26 +0330

    Fix created at

--- app/Http/Resources/UserSuperMinimalResource.php
+++ app/Http/Resources/UserSuperMinimalResource.php
@@ -5,21 +5,20 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserSuperMinimalResource extends JsonResource
-{
+class UserSuperMinimalResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
+    public function toArray(Request $request): array {
         return [
-            'id'       => $this->id,
-            'avatar'   => FileResource::make($this->avatar),
-            'name'     => $this->name,
-            'username' => $this->username,
-            'status'   => $this->status,
+            'id'         => $this->id,
+            'avatar'     => FileResource::make($this->avatar),
+            'name'       => $this->name,
+            'username'   => $this->username,
+            'status'     => $this->status,
+            'created_at' => $this->created_at->timestamp
 
         ];
     }
