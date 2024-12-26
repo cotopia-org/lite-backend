acceb4071f569f0260dbfbb87458fd7f8981af79
katerou22 <xkaterou22@gmail.com>
2024-12-27 02:08:44 +0330

    Fix last seen message.

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -28,6 +29,7 @@ class ChatResource extends JsonResource {
             'workspace_id'       => $this->workspace_id,
             'participants'       => UserSuperMinimalResource::collection($this->users),
             'last_message'       => MessageResource::make($this->lastMessage),
+            'last_seen_message'  => MessageResource::make(Message::find($chat_user->pivot->last_message_seen_id)),
             'unseens'            => $this->messages_count,
             'mentioned_messages' => $this->mentions_count,
             'muted'              => $chat_user === NULL ? FALSE : $chat_user->pivot->muted,
