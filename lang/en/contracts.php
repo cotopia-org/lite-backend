efa9ab698c09585ef3c94e5e3e3fdf3c56cca293
katerou22 <xkaterou22@gmail.com>
2024-12-29 18:36:39 +0330

    Fix contract.

--- lang/en/contracts.php
+++ lang/en/contracts.php
@@ -17,13 +17,21 @@ return [
         ],
 
         3 => [
-            'Presence hours' => [
-                "The second party agrees to be present on the **:workspace_name** for a minimum of **:min_hours** hours and maximum of **:max_hours** hours",
+            'Presence hours (minimum)' => [
+                "The second party agrees to be present on the **:workspace_name** for a minimum of **:min_hours** hours",
                 "If the working hours are less than the minimum hours of the contract, no payment will be made.",
+            ]
+        ],
+
+
+        4 => [
+            'Presence hours (maximum)' => [
+                "The second party agrees to be present on the **:workspace_name** for a maximum of **:max_hours** hours",
                 "If the working hours are more than the contract hours, payment will be made according to the maximum working hours.",
             ]
         ],
-        4 => [
+
+        5 => [
             'Payment Details' => [
                 "The first party agrees to deposit **:per_hour** USDT per hour of the second party's work to the account introduced by the second party.",
                 "The payment method is **:payment_method** , the second party will pay **:payment_period** to first party on payment address entered by first party **(:payment_address)**",
@@ -32,19 +40,19 @@ return [
             ]
         ],
 
-        5 => [
+        6 => [
             'Renewal' => [
                 "This contract will renew for another **:renewal_count** **:renew_time_period_type** automatically",
                 "If neither of the two parties has any objection to the contract extension until **:renew_notice** days before the end of the contract.",
                 "This means that any party that does not wish to renew the contract must notify the other party at least 10 days before the end of the contract."
             ]
         ],
-        6 => [
+        7 => [
             "Counting times" => [
                 "The first party must be online on schedule times added to contract and only times count in schedule"
             ]
         ],
-        7 => [
+        8 => [
             "Disclosure" => [
                 "This agreement is not confidential and the parties are allowed to publicly disclose its provisions."
             ]
