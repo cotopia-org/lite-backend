49051f24e1bb9855a9477d757a3a235836ffd200
Katerou22 <xkaterou22@gmail.com>
2025-01-28 16:44:27 +0330

    Test lotfi activity

--- app/Utilities/helpers.php
+++ app/Utilities/helpers.php
@@ -277,7 +277,6 @@ function isActivityInSchedule($schedule, $activity)
                 $start = $created_at->copy()->timezone($schedule->timezone)->setTimeFromTimeString($time->start);
                 if ($created_at->copy()->timezone($schedule->timezone)->between($start, $end)
                 ) {
-                    dd($created_at->copy()->timezone($schedule->timezone), $start, $end);
                     return TRUE;
                 }
 
